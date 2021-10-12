be1745e0686bb3fa1f199648a9626bf691ed9226
sudita07 <92347854+sudita07@users.noreply.github.com>
2021-10-11 20:02:30 -0700

    Add files via upload

---
+++ class12while.py
@@ -0,0 +1,90 @@
+# i=5
+# while i>=1:
+#      print("sudita",i)
+#      i=i-1
+# i=5
+# while i>=1:
+#     print("sudita",i)
+#     i=i-1
+# .........................................
+# take 10 integers from keyboard using loop and print their average value on the screen.
+# sum=0
+# i=10
+# while i>0:
+#      num=int(input("enter a number"))
+#      sum=sum+num
+#      i=i-1
+# print("average=",sum/10.0)
+# ...............................................
+# i=1
+# while i<=4:
+#      print(20*i)
+#      i=i+1
+# ................................
+# i=1
+# while i<=5:
+#      print ("*"*i)
+#      i=i+1
+# ..................................
+# i=1
+# j=2
+# while i>=1:
+#      a=""*j+"*"*i+""*j
+#      print(a)
+#      i=i+1
+#      j=j-1
+#      if i>5:
+#           break
+# i=3
+# j=1
+# while i>=1:
+#      a=""*j+"*"*i+""*j
+#      print(a)
+#      i=i-2
+     # j=j+1
+# ................................................
+# i=1
+# while i<=10:
+#      print(29*i)
+#      i=i+1
+# ......................................
+# i=1
+# while i<=10:
+#      print(50*i)
+#      i=i+1
+# .......................................
+# i=1
+# while i<=10:
+#      print(72*i)
+#      i=i+1
+# ....................................
+# i = 10
+# while i>=1:
+#     print("10"*i)
+#     i=i-2
+# ..........................................
+# i=1
+# while i<=4:
+#     print(25*i)
+#     i=i+1
+# ................................
+# .........................
+# o=10
+# while o>=4:
+#     print(2+o)
+#     o=o-1
+# ..........................
+# i=1
+# while i<=5:
+#     print(10-i)
+#     i=i+1
+# ....................
+# t=int(input("enter number"))
+# y=int(input("enter number"))
+# x=int(input("enter number"))
+# if t>y and t>x:
+#     print("largest= t")
+# if y>t and y>x:
+#      print("largest= y")
+# if x>t and x>y:
+#      print("largest= x")
